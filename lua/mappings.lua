bfc8e50290ae0069e0dbe611672a6daee292e4bf
John Kowalsky <john.kowalsky@petiq.com>
2024-03-06 14:33:53 -0700

    add mappings and fix esling_d error

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,12 +1,58 @@
-
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>w', ':w!<CR>')
-vim.keymap.set('i', 'jj', '<ESC>')
-vim.keymap.set('n', '<leader>q', ':q!<CR>')
-vim.keymap.set('n', '<leader>x', ':x!<CR>')
+vim.keymap.set("n", "<leader>w", ":w!<CR>")
+vim.keymap.set("i", "jj", "<ESC>")
+vim.keymap.set("n", "<leader>q", ":q!<CR>")
+vim.keymap.set("n", "<leader>x", ":x!<CR>")
 
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.mouse = "a"
+vim.opt.scrolloff = 10
+vim.opt.hlsearch = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.cursorline = true
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.keymap.set("n", "<ESC>", ":nohlsearch<CR>")
+
+-- line numbers
+vim.keymap.set("n", "<leader>n", ":set nu!<CR>")
+vim.keymap.set("n", "<leader>rn", ":set rnu!<CR>")
+
+-- go to beginning and end of line
+vim.keymap.set("i", "<C-b>", "<ESC>^i")
+vim.keymap.set("i", "<C-e>", "<End>")
+
+-- navigate within insert mode
+vim.keymap.set("i", "<C-h>", "<Left>")
+vim.keymap.set("i", "<C-l>", "<Right>")
+vim.keymap.set("i", "<C-j>", "<Down>")
+vim.keymap.set("i", "<C-k>", "<Up>")
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,10 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
+            vim.keymap.set('n', "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set('n', "<leader>fk", builtin.keymaps, {})
         end
     },
     {
